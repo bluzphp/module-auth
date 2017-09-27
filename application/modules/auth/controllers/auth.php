9857b0fe27333a6a99db07e32f4748434d4e90d6
Anton Shevchuk <AntonShevchuk@gmail.com>
2017-09-27 14:48:55 +0300

    Migrate Auth module to new version of HybridAuth

--- application/modules/auth/controllers/auth.php
+++ application/modules/auth/controllers/auth.php
@@ -1,31 +1,91 @@
 <?php
 /**
  * Auth controller
- *
- * @author yuklia
- * @created  05.05.15 17:30
  */
 namespace Application;
 
-use Application\Auth\AuthProvider;
 use Bluz\Controller\Controller;
+use Bluz\Proxy\Config;
 use Bluz\Proxy\Messages;
+use Bluz\Proxy\Response;
+use Bluz\Proxy\Router;
 
 /**
  * @param string $provider
- * @return false
+ *
+ * @return void
+ * @throws Exception
  */
 return function ($provider = '') {
     /**
      * @var Controller $this
      */
     try {
-        $auth = new AuthProvider($provider);
-        $auth->setIdentity($this->user());
-        $auth->authProcess();
-    } catch (Exception $e) {
+        // Get configuration
+        $config = Config::getData('auth', 'hybrid');
+        $provider = strtolower($provider);
+
+        // Check provider name
+        if (!array_key_exists($provider, $config['providers'])) {
+            throw new Exception('Invalid provider name');
+        }
+
+        $config['callback'] = Router::getFullUrl('auth', 'auth', ['provider' => $provider]);
+
+        // Feed configuration array to Hybridauth
+        $hybridauth = new \Hybridauth\Hybridauth($config);
+
+        // Attempt to authenticate users with a provider by name
+        $adapter = $hybridauth->authenticate(ucfirst($provider));
+
+        // Returns a boolean of whether the user is connected with provider
+        if ($adapter->isConnected()) {
+            // Retrieve the user's profile
+            $profile = $adapter->getUserProfile();
+
+            // Access token from provider
+            $accessToken = $adapter->getAccessToken();
+
+            // Check authRow
+            $authRow = Auth\Table::getAuthRow($provider, $profile->identifier);
+
+            // Inspect profile's public attributes
+            if ($this->user()) {
+                if ($authRow) {
+                    Messages::addNotice('You have already linked to `%s`', $provider);
+                } else {
+                    // Create token and link it with user profile
+                    $authRow = new Auth\Row();
+                    $authRow->userId = $this->user()->id;
+                    $authRow->provider = $provider;
+                    $authRow->foreignKey = $profile->identifier;
+                    $authRow->tokenSecret = $accessToken['access_token_secret'] ?? '';
+                    $authRow->tokenType = $accessToken['token_type'] ?? Auth\Table::TYPE_ACCESS;
+                    Messages::addNotice('Your account was linked to `%s` successfully!', $provider);
+                }
+                // Update access token
+                $authRow->token = $accessToken['access_token'];
+                $authRow->save();
+                Response::redirectTo('users', 'profile');
+            } else {
+                // Authenticate in the application
+                if ($authRow) {
+                    // Try to login
+                    $user = Users\Table::findRow($authRow->userId);
+                    Auth\Table::tryLogin($user);
+                    Messages::addNotice('You are signed');
+                } else {
+                    // User not found
+                    Messages::addError('Not found linked profile');
+                    Response::redirectTo('users', 'signin');
+                }
+            }
+
+            // Disconnect the adapter
+            $adapter->disconnect();
+        }
+    } catch (\Hybridauth\Exception\Exception $e) {
         Messages::addError($e->getMessage());
     }
-
-    return false;
+    Response::redirectTo('index', 'index');
 };

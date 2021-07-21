072e98547cf91196a879db35521037d8031a0cd2
Anton Shevchuk <antonshevchuk@gmail.com>
2021-07-21 18:01:56 +0300

    Updated Auth

--- application/modules/auth/controllers/auth.php
+++ application/modules/auth/controllers/auth.php
@@ -72,8 +72,7 @@ return function ($provider = '') {
                 Response::redirectTo('users', 'profile');
             } elseif ($authRow) {
                 // Try to login
-                $user = Users\Table::findRow($authRow->userId);
-                Auth\Table::tryLogin($user);
+                Auth\Provider\Token::login($authRow);
                 Messages::addNotice('You are signed');
             } else {
                 // User not found
